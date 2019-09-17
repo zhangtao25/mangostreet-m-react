978536c91edafee16d003b81ab32e4c97ebdbcba
Allen Zhang <tzhangm@ctrip.com>
2019-09-17 19:08:34 +0800

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {HashRouter} from 'react-router-dom'
+import {HashRouter,Switch,Route,withRouter} from 'react-router-dom'
 import AppTabBar from './layouts/AppTabBar'
 import routes from './router/router.config'
 import './assets/css/App.css'
 import renderRoutes from "./router/renderRoutes";
 import {ActivityIndicator} from "antd-mobile";
 import {inject, observer} from "mobx-react";
+import {TransitionGroup,CSSTransition} from 'react-transition-group'
 
 interface State {
 
@@ -14,6 +15,19 @@ interface State {
 export interface Props {
   store:any
 }
+
+const Routes = withRouter(({location}) => (
+  <TransitionGroup className={'router-wrapper'}>
+    <CSSTransition
+      timeout={5000}
+      classNames={'fade'}
+      key={location.pathname}
+      unmountOnExit={true}
+    >
+      {renderRoutes(routes,false)}
+    </CSSTransition>
+  </TransitionGroup>
+));
 
 // 观察者
 @inject('store')
@@ -26,7 +40,7 @@ class App extends React.Component<Props, State> {
           <ActivityIndicator toast animating={this.props.store.activityIndicatorStatus} text="正在加载"/>
           <AppTabBar/>
           <div className='wrap'>
-            {renderRoutes(routes,false)}
+            <Routes/>
           </div>
         </HashRouter>
       </div>
